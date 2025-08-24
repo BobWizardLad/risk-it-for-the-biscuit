0573a20c2b36647bddf24482b05638d8538bd846
Rylan Davidson <RylanDavidson3@gmail.com>
2025-08-24 13:51:15 -0500

    Enemies wait to be activated
    When enemies get active they can kill you and when you die they get de-activated

--- bake-hard/scenes/enemy.gd
+++ bake-hard/scenes/enemy.gd
@@ -3,18 +3,55 @@ extends CharacterBody2D
 
 @onready var sprite = $Sprites
 @onready var animationplayer = $AnimationPlayer
+@onready var vision = $Vision
 
 var is_dead = false
 
+@export var speed := 80
+
 func _ready() -> void:
-    pass
+	set_process(false)
+	# Subscribe to parent's signal
+	if get_parent().has_signal("activate_enemies"):
+		get_parent().connect("activate_enemies", _activate)
+
+	if get_parent().has_signal("de_activate_enemies"):
+		get_parent().connect("de_activate_enemies", _deactivate)
 
 func take_attack():
-    if !is_dead:
-        print_debug("Enemy took an attack")
+	if !is_dead and is_processing():
+		print_debug("Enemy took an attack")
 
-        # When hit by an attack, enemy will fall to a deactivated state
-        animationplayer.play("damage")
-        animationplayer.queue("death")
-        is_dead = true
-        set_process(false)
+		# When hit by an attack, enemy will fall to a deactivated state
+		animationplayer.play("damage")
+		animationplayer.queue("death")
+		is_dead = true
+		set_process(false)
+
+func _process(delta: float) -> void:
+	if is_dead:
+		return
+
+	# Check if the player is in the enemy's vision
+	var overlapping_bodies = vision.get_overlapping_bodies()
+	for body in overlapping_bodies:
+		if body is Player:
+			# Face the player
+			look_at(body.global_position)
+			# Walk towards the player
+			var direction = (body.global_position - global_position).normalized()
+			velocity = direction * speed
+			move_and_slide()
+
+			var collision = get_last_slide_collision()
+			if collision and collision.get_collider() is Player:
+				collision.get_collider().take_attack()
+			pass
+
+func _activate() -> void:
+	print("Activating enemy")
+	set_process(true)
+	
+func _deactivate() -> void:
+	print("Deactivating enemy")
+	set_process(false)
