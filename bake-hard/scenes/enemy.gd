75da55f22eab2539166168e9c6a35814d994b3aa
Rylan Davidson <RylanDavidson3@gmail.com>
2025-08-24 14:50:58 -0500

    Enemies are slightly Smarter
    They will not path if they cant see you

--- bake-hard/scenes/enemy.gd
+++ bake-hard/scenes/enemy.gd
@@ -1,6 +1,8 @@
 class_name Enemy
 extends CharacterBody2D
 
+@export var signal_control: Node2D
+
 @onready var sprite = $Sprites
 @onready var animationplayer = $AnimationPlayer
 @onready var vision = $Vision
@@ -12,11 +14,11 @@ var is_dead = false
 func _ready() -> void:
 	set_process(false)
 	# Subscribe to parent's signal
-	if get_parent().has_signal("activate_enemies"):
-		get_parent().connect("activate_enemies", _activate)
+	if signal_control.has_signal("activate_enemies"):
+		signal_control.connect("activate_enemies", _activate)
 
-	if get_parent().has_signal("de_activate_enemies"):
-		get_parent().connect("de_activate_enemies", _deactivate)
+	if signal_control.has_signal("de_activate_enemies"):
+		signal_control.connect("de_activate_enemies", _deactivate)
 
 func take_attack():
 	if !is_dead and is_processing():
@@ -36,17 +38,27 @@ func _process(delta: float) -> void:
 	var overlapping_bodies = vision.get_overlapping_bodies()
 	for body in overlapping_bodies:
 		if body is Player:
-			# Face the player
-			look_at(body.global_position)
-			# Walk towards the player
-			var direction = (body.global_position - global_position).normalized()
-			velocity = direction * speed
-			move_and_slide()
+			var start_pos = global_position
+			var end_pos = body.global_position
+			var space_state = get_world_2d().direct_space_state
+			var ray_params = PhysicsRayQueryParameters2D.new()
+			ray_params.from = start_pos
+			ray_params.to = end_pos
+			ray_params.exclude = [self]
+			var result = space_state.intersect_ray(ray_params)
+			if result:
+				# You can access result.position, result.collider, etc.
+				if result.collider is Player:
+					var direction = (body.global_position - global_position).normalized()
+					# Face the player
+					look_at(body.global_position)
+					# Walk towards the player
+					velocity = direction * speed
+					move_and_slide()
 
-			var collision = get_last_slide_collision()
-			if collision and collision.get_collider() is Player:
-				collision.get_collider().take_attack()
-			pass
+					var collision = get_last_slide_collision()
+					if collision and collision.get_collider() is Player:
+						collision.get_collider().take_attack()
 
 func _activate() -> void:
 	print("Activating enemy")
