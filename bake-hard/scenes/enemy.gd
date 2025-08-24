39bb4c09682423583311867ca5bbd4f906be097d
bobwizardlad <bobwizardlad@gmail.com>
2025-08-24 11:37:36 -0500

    Implement enemy damage and death animations

--- bake-hard/scenes/enemy.gd
+++ bake-hard/scenes/enemy.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 @onready var sprite = $Sprites
+@onready var animationplayer = $AnimationPlayer
 
 func _ready() -> void:
-    pass
+    take_attack()
 
 func take_attack():
     print_debug("Enemy took an attack")
 
     # When hit by an attack, enemy will fall to a deactivated state
-    sprite.modulate = Color(1, 1, 1, 0.2)
+    animationplayer.play("damage")
+    animationplayer.queue("death")
     
