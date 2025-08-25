a9ec07ce67224be14acbc22aee9f153155ae134e
bobwizardlad <bobwizardlad@gmail.com>
2025-08-25 17:50:25 -0500

    refactor display_case scene and script to display prompt text when nearby

--- bake-hard/scripts/display_case.gd
+++ bake-hard/scripts/display_case.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
+@onready var interact_label = $InteractLabel
+
 signal level_begin
 
 var player_in_area = false
 
 func _ready() -> void:
-	pass
+	interact_label.hide()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fire"):
@@ -18,8 +20,10 @@ func break_display_case():
 
 func _on_start_area_body_entered(body: Node) -> void:
 	if body is Player:
+		interact_label.show()
 		player_in_area = true
 
 func _on_start_area_body_exited(body: Node) -> void:
 	if body is Player:
+		interact_label.hide()
 		player_in_area = false
