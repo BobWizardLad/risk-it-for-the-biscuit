a426245f4def80041079163fd2cb6497aaa5c90e
bobwizardlad <bobwizardlad@gmail.com>
2025-08-24 20:34:37 -0500

    implement display case interactions

--- bake-hard/scripts/display_case.gd
+++ bake-hard/scripts/display_case.gd
@@ -2,9 +2,24 @@ extends Node2D
 
 signal level_begin
 
+var player_in_area = false
+
 func _ready() -> void:
-	break_display_case()
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fire"):
+		if player_in_area:
+			break_display_case()
 
 func break_display_case():
 	# Do stuff to telegraph the case breaking and the level start
 	level_begin.emit()
+
+func _on_start_area_body_entered(body: Node) -> void:
+	if body is Player:
+		player_in_area = true
+
+func _on_start_area_body_exited(body: Node) -> void:
+	if body is Player:
+		player_in_area = false
--- bake-hard/scripts/player.gd
+++ bake-hard/scripts/player.gd
@@ -22,7 +22,7 @@ func take_attack():
         is_dead = true
         player_died.emit()
 
-func _input(event):
+func _unhandled_input(event):
     if !is_dead:
         velocity = Vector2.ZERO
         if Input.is_action_pressed("ui_right"):
