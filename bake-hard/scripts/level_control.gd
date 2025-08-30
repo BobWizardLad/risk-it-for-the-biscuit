154703451833e2f7f79c8a9ccb5c551eaa53c490
bobwizardlad <bobwizardlad@gmail.com>
2025-08-30 11:40:27 -0500

    Music staging for gameplay

--- bake-hard/scripts/level_control.gd
+++ bake-hard/scripts/level_control.gd
@@ -7,21 +7,43 @@ signal de_activate_enemies
 @export var gnome_count: RichTextLabel
 @export var reset_button: Button
 @export var objective_anchor: ObjectivesAnchor
+@export var jukebox: AudioStreamPlayer
+@export var passive_music_track: AudioStream
+@export var active_music_track: AudioStream
+@export var aftermath_music_track: AudioStream
 @onready var player = $Player
 
+var gnomes_defeated: int
+var total_gnomes: int
 
 func _ready() -> void:
 	player.player_died.connect(_on_player_player_died)
-	
+
+	# Gnomes in level; get count from scene group
+	gnomes_defeated = 0
+	total_gnomes = get_tree().get_node_count_in_group("Enemy")
+
+	# Audio setup
+	jukebox.stream = passive_music_track
+	jukebox.play()
+
 	# Objectives for each level
 	objective_anchor.add_objective("Biscuit", "Find and steal the biscuit")
 	objective_anchor.add_objective("Gnomes", "Serve the gnomes in your way")
 
+func _process(_delta: float) -> void:
+	if gnomes_defeated == total_gnomes:
+		objective_anchor.remove_objective("Gnomes")
+		jukebox.stream = aftermath_music_track
+		jukebox.play()
+
+
 func _on_activate_pressed() -> void:
+	# Start the active music
+	jukebox.stream = active_music_track
+	jukebox.play()
 
 	activate_enemies.emit()
-	pass # Replace with function body.
-
 
 func _on_de_activate_pressed() -> void:
 	de_activate_enemies.emit()
@@ -32,6 +54,8 @@ func _on_enemy_death() -> void:
 
 func _on_player_player_died() -> void:
 	de_activate_enemies.emit()
+	jukebox.stream = aftermath_music_track
+	jukebox.play()
 	death_text.visible = true
 	gnome_count.text = str("Gnome count was: " + str(GLOBAL_VARIABLES.gnome_count) + " gnomes handled.")
 	gnome_count.visible = true
@@ -43,3 +67,6 @@ func _on_pickup_picked_up(pickup_name:String) -> void:
 	if pickup_name == "biscuit":
 		objective_anchor.remove_objective("Biscuit")
 		objective_anchor.add_objective("Escape", "Get out")
+
+func _on_activate_custscene_begin() -> void:
+	jukebox.stop()
